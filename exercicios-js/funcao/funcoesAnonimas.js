8b9fd909a9385fec56f864e08cd729b4dbd051d8
Fernando <fsazevedo09@gmail.com>
2022-05-24 20:02:47 -0300

    Função Anonima

---
+++ exercicios-js/funcao/funcoesAnonimas.js
@@ -0,0 +1,19 @@
+const soma = (x, y) => x + y // shorthand
+
+const imprimirResultado = (a, b, operacao = soma) => console.log(operacao(a, b)) // shorthand
+
+imprimirResultado(3, 4) 
+imprimirResultado(3, 4, soma) 
+imprimirResultado(3, 4, function (x, y) {
+    return x - y
+})
+imprimirResultado(3, 4, (x, y) => x * y)
+
+const pessoa = {
+    falar: function () {
+        console.log('Opa')
+    }
+}
+
+pessoa.falar()
+// função anonima é uma função sem nome...
